d9429ae902bd123afdf42e75157eca7a4021e4c3
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-07-09 13:55:44 -0700

    Add slash command options

--- src/main/java/com/technovision/tutorialbot/commands/CommandManager.java
+++ src/main/java/com/technovision/tutorialbot/commands/CommandManager.java
@@ -1,12 +1,15 @@
 package com.technovision.tutorialbot.commands;
 
-import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -39,6 +42,30 @@ public class CommandManager extends ListenerAdapter {
             }
             event.getHook().sendMessage(response).queue();
         }
+        else if (command.equals("say")) {
+            // Get message option
+            OptionMapping messageOption = event.getOption("message");
+            String message = messageOption.getAsString();
+
+            // Get channel option if specified
+            MessageChannel channel;
+            OptionMapping channelOption = event.getOption("channel");
+            if (channelOption != null) {
+                channel = channelOption.getAsMessageChannel();
+            } else {
+                channel = event.getChannel();
+            }
+
+            // Send message
+            channel.sendMessage(message).queue();
+            event.reply("Your message was sent!").setEphemeral(true).queue();
+        }
+        else if (command.equals("giverole")) {
+            Member member = event.getOption("user").getAsMember();
+            Role role = event.getOption("role").getAsRole();
+            event.getGuild().addRoleToMember(member, role).queue();
+            event.reply(member.getAsMention() + " has been given the " + role.getAsMention() + " role!").queue();
+        }
     }
 
     /**
@@ -50,6 +77,16 @@ public class CommandManager extends ListenerAdapter {
         List<CommandData> commandData = new ArrayList<>();
         commandData.add(Commands.slash("welcome", "Get welcomed by the bot"));
         commandData.add(Commands.slash("roles", "Display all roles on the server"));
+
+        OptionData option1 = new OptionData(OptionType.STRING, "message", "The message you want the bot to say", true);
+        OptionData option2 = new OptionData(OptionType.CHANNEL, "channel", "The channel you want to send this message in")
+                .setChannelTypes(ChannelType.TEXT, ChannelType.NEWS, ChannelType.GUILD_PUBLIC_THREAD);
+        commandData.add(Commands.slash("say", "Make the bot say a message").addOptions(option1, option2));
+
+        OptionData option3 = new OptionData(OptionType.USER, "user", "The user to give the role to", true);
+        OptionData option4 = new OptionData(OptionType.ROLE, "role", "The role to be given", true);
+        commandData.add(Commands.slash("giverole", "Give a user a role").addOptions(option3, option4));
+
         event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 
